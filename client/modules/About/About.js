ab6396740ca3239a6b08c38e7e1cf8adc62563bd
Tomasz Paterka <tomekpaterka@gmail.com>
2018-06-01 11:23:48 +0200

    add "home" and "about" modules

---
+++ client/modules/About/About.js
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About me</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
